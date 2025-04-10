b05b3a4b5a07f1759b5cdce681286808aae143c6
RupanR <rupanmentor@gmail.com>
2025-04-10 20:31:39 +0530

    completed

---
+++ script.js
@@ -0,0 +1,27 @@
+//! Spread Operator
+
+/*
+const arr = ["guvi","geek","fsd"]
+console.log(...arr); 
+*/
+
+/*
+const arr = ["bob","alice"]
+const arr1 = ["leo","smith"]
+const arr2 = [...arr,...arr1]
+console.log(arr2);
+*/
+
+//! Rest parameter
+/*
+function test (...c){
+    let even=[];
+   for(let i=0; i<c.length;i++){
+     if(c[i]%2 ===0){
+        even.push(c[i])
+     }
+   }
+   return even
+}
+console.log(test(10,11,13,15,14,16,18));
+*/
